4546c7a165be6586ea7e32317921d9627830c6ad
DHelloper <wjdehgud5769@naver.com>
2023-01-17 10:30:33 +0900

    test

--- src/main/java/com/example/jenkinstest/controller/Hello.java
+++ src/main/java/com/example/jenkinstest/controller/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String test() {
         return "Hello World!";
     }
